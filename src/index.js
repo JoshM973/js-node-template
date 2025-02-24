d13333adc387ac3ed3c2d6c6cb95cade86ea0706
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-02-24 14:39:52 -0600

    Add function practice examples

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
-var x = 3;
+const student1 = {
+	name: "Sarah",
+	course: "JavaScript",
+	grade: 95,
+};
 
-console.log(x);
+const student2 = {
+	name: "John",
+	course: "Python",
+	grade: 88,
+};
+
+const student3 = {
+	name: "Alice",
+	course: "C++",
+	grade: 92,
+};
+
+function displayStudentInfo(student) {
+	return `${student.name} is taking ${student.course} and expects to earn a ${student.grade}%.`;
+}
+
+console.log(displayStudentInfo(student1));
+console.log(displayStudentInfo(student2));
+console.log(displayStudentInfo(student3));
+
+function add2Nums(x, y) {
+	return x + y;
+}
+
+const sum = add2Nums(200, 300);
+console.log(sum);
