5e85e9538e2bf5af7e6e264ea61b07124950deb1
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-02-23 20:32:13 -0600

    add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,9 @@
+const name = "Josh Marquardt";
+const course = "Web Development I";
+const grade = 85;
+const statement =
+	name + " is taking " + course + " and expects to earn a " + grade + "%.";
+console.log(statement);
+
+const betterStatement = `${name} is taking ${course} and expects to earn a ${grade}%.`;
+console.log(betterStatement);
