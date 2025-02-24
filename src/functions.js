5c80f0d355986e7a869e4e5b1d3eb7d6a42a2244
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-02-24 14:50:39 -0600

    Format a username

--- src/functions.js
+++ src/functions.js
@@ -6,3 +6,12 @@ function calculateTotal(order) {
 	return order.price * order.quantity;
 }
 console.log(calculateTotal(order));
+
+const name = {
+	first: "John",
+	last: "Doe",
+};
+function formatUsername(firstName, lastName) {
+	return `${lastName}, ${firstName}`;
+}
+console.log(formatUsername("John", "Doe"));
