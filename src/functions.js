67b237a2ae73936ea284b8da37b39650e1b6801a
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-02-24 14:53:11 -0600

    Combine two strings with a space between

--- src/functions.js
+++ src/functions.js
@@ -15,3 +15,12 @@ function formatUsername(firstName, lastName) {
 	return `${lastName}, ${firstName}`;
 }
 console.log(formatUsername("John", "Doe"));
+
+const strings = {
+	str1: "Hello",
+	str2: "World",
+};
+function combineStrings(str1, str2) {
+	return `${str1} ${str2}`;
+}
+console.log(combineStrings("Hello", "World"));
