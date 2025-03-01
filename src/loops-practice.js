4a87dae2ebc4aa75b5241f23b9b7349ee96271ac
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-03-01 14:19:30 -0600

    for and while loops count to 10

---
+++ src/loops-practice.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+	console.log(i);
+}
+
+let j = 1;
+while (j < 11) {
+	console.log(j);
+	j++;
+}
+// I used <= 10 in the for loop and < 11 in the while loop to show that they can be used for the same result.
