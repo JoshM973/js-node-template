abb1082b9d5a594de60df2b27197061b2404d73e
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-03-01 15:44:42 -0600

    Working with Objects in Arrays

--- src/loops-practice.js
+++ src/loops-practice.js
@@ -19,3 +19,17 @@ for (let i = 0; i < games.length; i++) {
 	console.log(`Game ${i + 1}: ${games[i]}`);
 }
 // I started with i = 0 because thats where arrays begin and used i < games.length to make sure it loops through the entire array without going over.
+
+const players = [
+	{ name: "Soap", kills: 10, deaths: 0 },
+	{ name: "Ghost", kills: 20, deaths: 3 },
+	{ name: "Price", kills: 15, deaths: 12 },
+];
+for (let i = 0; i < players.length; i++) {
+	const player = players[i];
+	const killDeathRatio = player.kills / Math.max(1, player.deaths);
+	console.log(
+		` ${player.name}: ${player.kills} Kills, ${player.deaths}deaths, K/D Ratio: ${killDeathRatio.toFixed(2)}`,
+	);
+}
+// I used Math.max(1, player.deaths) to prevent division by zero and ensure the K/D ratio is calculated correctly.
