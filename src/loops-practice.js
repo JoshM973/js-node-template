0a40de78e722d5ff6e3662ff6eeb1f6d4242531d
Josh Marquardt <joshua.marquardt001@students.swic.edu>
2025-03-01 15:27:29 -0600

    Array Iteration, favorite games

--- src/loops-practice.js
+++ src/loops-practice.js
@@ -8,3 +8,14 @@ while (j < 11) {
 	j++;
 }
 // I used <= 10 in the for loop and < 11 in the while loop to show that they can be used for the same result.
+const games = [
+	"Minecraft",
+	"Super Mario",
+	"Call of Duty",
+	"Pokemon",
+	"Stardew Valley",
+];
+for (let i = 0; i < games.length; i++) {
+	console.log(`Game ${i + 1}: ${games[i]}`);
+}
+// I started with i = 0 because thats where arrays begin and used i < games.length to make sure it loops through the entire array without going over.
